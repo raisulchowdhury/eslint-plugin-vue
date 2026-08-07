5d6157a1bdb784137aba6811560d807d3b12fa81
Raisul Chowdhury <34920788+raisulchowdhury@users.noreply.github.com>
2026-08-07 13:54:16 -0700

    Test default languages without special handling

--- tests/lib/rules/block-lang-explicit-default.test.ts
+++ tests/lib/rules/block-lang-explicit-default.test.ts
@@ -13,16 +13,29 @@ tester.run('block-lang (explicit default language)', rule, {
   valid: [
     {
       code: '<template></template><script lang="js"></script>',
-      options: [{ script: { lang: 'js', allowNoLang: false } }]
+      options: [{ script: { lang: 'js' } }]
+    },
+    {
+      code: '<template></template><script></script>',
+      options: [{ script: { lang: 'js', allowNoLang: true } }]
     }
   ],
   invalid: [
     {
       code: '<template></template><script></script>',
-      options: [{ script: { lang: 'js', allowNoLang: false } }],
+      options: [{ script: { lang: 'js' } }],
       errors: [
         {
           message: "The 'lang' attribute of '<script>' is missing."
+        }
+      ]
+    },
+    {
+      code: '<template></template><script lang="ts"></script>',
+      options: [{ script: { lang: 'js' } }],
+      errors: [
+        {
+          message: "Only 'js' can be used for the 'lang' attribute of '<script>'."
         }
       ]
     }
